ff87c6f08622d8f11eb9f2f19c33f2421292085d
Anand Angalig <anandangalig@gmail.com>
2019-04-10 10:15:40 -0400

    add continent colors to gap minder: year 1

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -4,15 +4,6 @@
  *    Project 2 - Gapminder Clone
  */
 
-d3.json('data/data.json').then(function(data) {
-  let firstYear = data[0];
-  firstYear.countries = firstYear.countries.filter(country => {
-    return country.income && country.life_exp;
-  });
-
-  update(firstYear);
-});
-
 const margin = { left: 80, right: 20, top: 50, bottom: 100 };
 const width = 700 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
@@ -30,12 +21,14 @@ const y = d3
   .scaleLinear()
   .range([height, 0])
   .domain([0, 90]);
+
 const x = d3
   .scaleLog()
   .base(10)
   .range([0, width])
   .domain([300, 150000]);
-const area = d3
+
+const getRadius = d3
   .scaleLinear()
   .range([5, 40])
   .domain([2000, 1400000000]);
@@ -43,6 +36,8 @@ const area = d3
 //   .scaleLinear()
 //   .range([25 * Math.PI, 1500 * Math.PI])
 //   .domain([2000, 1400000000]);
+
+const getContinentColor = d3.scaleOrdinal(d3.schemeCategory10);
 
 // X axis
 const xAxisCall = d3
@@ -53,11 +48,13 @@ g.append('g')
   .attr('class', 'x axis')
   .attr('transform', 'translate(0,' + height + ')')
   .call(xAxisCall);
+
 // Y axis
 const yAxisCall = d3.axisLeft(y);
 g.append('g')
   .attr('class', 'y axis')
   .call(yAxisCall);
+
 // labels:
 const xLabel = g
   .append('text')
@@ -66,6 +63,7 @@ const xLabel = g
   .attr('text-anchor', 'middle')
   .attr('font-size', 25)
   .text('Income');
+
 const yLabel = g
   .append('text')
   .attr('x', -(height / 2)) // rotating affects the X,Y orientation, hence using height for X
@@ -78,7 +76,9 @@ const yLabel = g
 // update pattern
 const update = data => {
   const selection = g.selectAll('circle').data(data.countries);
+
   selection.exit().remove();
+
   selection
     .enter()
     .append('circle')
@@ -90,6 +90,24 @@ const update = data => {
 	})
 	// .attr("r", (d)=>{ return Math.sqrt(area(d.population) / Math.PI) });
     .attr('r', d => {
-      return area(d.population);
-    });
+      return getRadius(d.population);
+    })
+	.attr('fill', d => {
+      return getContinentColor(d.continent);
+    } );
 };
+
+d3.json('data/data.json').then(function(data) {
+  let firstYear = data[0];
+
+  firstYear.countries = firstYear.countries.filter(country => {
+    return country.income && country.life_exp;
+  });
+
+  firstYear.countries.map(country=>{
+	  country.income = +country.income;
+	  country.life_exp = +country.life_exp;
+  })
+
+  update(firstYear);
+});

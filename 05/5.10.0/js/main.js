55c41f932e067a2bde5ba47e9b0d64978c2760de
Anand Angalig <anandangalig@gmail.com>
2019-04-10 13:06:33 -0400

    add interval loading to update data with each year point

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -16,7 +16,7 @@ const svg = d3
 // main group:
 const g = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// scales:
+// scales: ============================================================
 const y = d3
   .scaleLinear()
   .range([height, 0])
@@ -37,9 +37,9 @@ const getRadius = d3
 //   .range([25 * Math.PI, 1500 * Math.PI])
 //   .domain([2000, 1400000000]);
 
-const getContinentColor = d3.scaleOrdinal(d3.schemeCategory10);
+const getContinentColor = d3.scaleOrdinal(d3.schemePastel1); // domain is omitted scaleOrdinal([[domain], range])
 
-// X axis
+// X, Y axes ============================================================
 const xAxisCall = d3
   .axisBottom(x)
   .tickValues([400, 4000, 40000])
@@ -49,23 +49,20 @@ g.append('g')
   .attr('transform', 'translate(0,' + height + ')')
   .call(xAxisCall);
 
-// Y axis
 const yAxisCall = d3.axisLeft(y);
 g.append('g')
   .attr('class', 'y axis')
   .call(yAxisCall);
 
-// labels:
-const xLabel = g
-  .append('text')
+// labels: =============================================================
+g.append('text')
   .attr('x', width / 2)
   .attr('y', height + 60)
   .attr('text-anchor', 'middle')
   .attr('font-size', 25)
   .text('Income');
 
-const yLabel = g
-  .append('text')
+g.append('text')
   .attr('x', -(height / 2)) // rotating affects the X,Y orientation, hence using height for X
   .attr('y', -60) // the origin point is also flipped, thus using negative values
   .attr('font-size', 25)
@@ -73,41 +70,54 @@ const yLabel = g
   .attr('transform', 'rotate(-90)')
   .text('Life Expectancy');
 
-// update pattern
+// update pattern ======================================================
+// prettier-ignore
 const update = data => {
-  const selection = g.selectAll('circle').data(data.countries);
+  const selection = g.selectAll('circle').data(data, function (d) { return d.country; });
+  // the second parameter is a function that sets the key to each data point to keep the binding consistent to specific DOM elements  (i.e. paint the same continent with same color with each interval)
 
   selection.exit().remove();
 
   selection
     .enter()
     .append('circle')
-    .attr('cy', d => {
-      return y(d.life_exp);
-    })
-    .attr('cx', d => {
-      return x(d.income);
-	})
-	// .attr("r", (d)=>{ return Math.sqrt(area(d.population) / Math.PI) });
-    .attr('r', d => {
-      return getRadius(d.population);
-    })
-	.attr('fill', d => {
+    .attr('fill', d => {
       return getContinentColor(d.continent);
-    } );
+    })
+    .merge(selection)
+      .attr('cy', d => {
+        return y(d.life_exp);
+      })
+      .attr('cx', d => {
+        return x(d.income);
+      })
+      // .attr("r", (d)=>{ return Math.sqrt(area(d.population) / Math.PI) });
+      .attr('r', d => {
+        return getRadius(d.population);
+      })
+      .attr('data-country-name', (d)=>d.country);
 };
 
+// load data and graph: ================================================
 d3.json('data/data.json').then(function(data) {
-  let firstYear = data[0];
-
-  firstYear.countries = firstYear.countries.filter(country => {
-    return country.income && country.life_exp;
+  // data is an array of 215 objects
+  // Each object has countries array (254) and a year string (1800-2014)
+  // Each country is an object with continent, country, income, life_exp, and population
+  const cleanedData = data.map(each => {
+    return each.countries.filter(country => {
+      return country.income && country.life_exp;
+    });
   });
 
-  firstYear.countries.map(country=>{
-	  country.income = +country.income;
-	  country.life_exp = +country.life_exp;
-  })
+  let arrayPosition = 0;
 
-  update(firstYear);
+  d3.interval(() => {
+    if (arrayPosition == 214) {
+      arrayPosition = 0;
+    } else {
+      arrayPosition++;
+    }
+
+    update(cleanedData[arrayPosition]);
+  }, 200);
 });
